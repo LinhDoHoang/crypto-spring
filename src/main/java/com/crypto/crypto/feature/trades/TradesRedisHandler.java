eefdc74127b2029196be8e16e77d33c7d7a4b9b1
LinhDoHoangDSV <linhdh@dgroup.co>
2026-08-23 21:16:23 +0700

    feat: complete websocket setup for single instance

--- src/main/java/com/crypto/crypto/feature/trades/TradesRedisHandler.java
+++ src/main/java/com/crypto/crypto/feature/trades/TradesRedisHandler.java
@@ -3,12 +3,19 @@ package com.crypto.crypto.feature.trades;
 import com.crypto.crypto.config.redis.RedisMessageHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TradesRedisHandler implements RedisMessageHandler {
 
     private static final Logger log = LoggerFactory.getLogger(TradesRedisHandler.class);
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public TradesRedisHandler(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
 
     @Override
     public String channel() {
@@ -17,6 +24,7 @@ public class TradesRedisHandler implements RedisMessageHandler {
 
     @Override
     public void handle(String jsonPayload) {
-        log.info("Received trade: {}", jsonPayload);
+        log.debug("Received trade: {}", jsonPayload);
+        messagingTemplate.convertAndSend("/topic/trades", jsonPayload);
     }
 }
